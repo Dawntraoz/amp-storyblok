2764effed44841d542f561fc881f4595af56d47c
Dawntraoz <albasilventefu@gmail.com>
2021-01-09 20:01:30 +0100

    Update packages

--- tailwind.js
+++ tailwind.js
@@ -7,6 +7,12 @@ module.exports = {
   },
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     container: {
       center: true,
       padding: '1rem'
